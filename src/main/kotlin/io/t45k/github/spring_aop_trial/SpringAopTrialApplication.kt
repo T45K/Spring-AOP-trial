2a5c69c986066951736ac0f75ef6a7fd1ee732ac
T45K <tasktas9@gmail.com>
2023-08-01 22:41:44 +0900

    add spel

--- src/main/kotlin/io/t45k/github/spring_aop_trial/SpringAopTrialApplication.kt
+++ src/main/kotlin/io/t45k/github/spring_aop_trial/SpringAopTrialApplication.kt
@@ -3,9 +3,12 @@ package io.t45k.github.spring_aop_trial
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.reflect.MethodSignature
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.repository.query.Param
 import org.springframework.expression.spel.standard.SpelExpressionParser
+import org.springframework.expression.spel.support.StandardEvaluationContext
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
@@ -35,7 +38,8 @@ class SampleController(private val sampleService: SampleService) {
 @Service
 class SampleService {
     @SampleAnnotation("now")
-    fun sample(input: SampleInput) {
+    @SampleAnnotation2("#input.now")
+    fun sample(@Param("input") input: SampleInput) {
     }
 }
 
@@ -44,6 +48,7 @@ data class SampleInput(val now: LocalDateTime) : SampleInterface
 interface SampleInterface
 
 annotation class SampleAnnotation(val spel: String)
+annotation class SampleAnnotation2(val spel: String)
 
 @Component
 @Aspect
@@ -51,7 +56,7 @@ class SampleAdvice {
     private val parser = SpelExpressionParser()
 
     @Before("@annotation(sampleAnnotation)")
-    fun exec(joinPoint: JoinPoint, sampleAnnotation: SampleAnnotation) {
+    fun exec1(joinPoint: JoinPoint, sampleAnnotation: SampleAnnotation) {
         val now = parser.parseExpression(sampleAnnotation.spel)
             .getValue(joinPoint.args[0])
             as LocalDateTime
@@ -59,8 +64,19 @@ class SampleAdvice {
     }
 
     @Before("execution(* *(SampleInput, ..)) && args(input, ..)")
-    fun exec(joinPoint: JoinPoint, input: SampleInput) {
+    fun exec2(joinPoint: JoinPoint, input: SampleInput) {
         val now = input.now
+        println(now)
+    }
+
+    @Before("@annotation(sampleAnnotation)")
+    fun exec3(joinPoint: JoinPoint, sampleAnnotation: SampleAnnotation2) {
+        val context = StandardEvaluationContext().apply {
+            setVariable((joinPoint.signature as MethodSignature).parameterNames[0], joinPoint.args[0])
+        }
+        val now = parser.parseExpression(sampleAnnotation.spel)
+            .getValue(context)
+            as LocalDateTime
         println(now)
     }
 }
